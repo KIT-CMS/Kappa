630b3a7c47c79f6a427e992df7729632b423b02a
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2018-02-01 15:39:57 +0100

    Skimming/scripts: adding a minimal checkout script for 94X (2017 data & MC)

---
+++ Skimming/scripts/checkoutCmssw94xPackagesForSkimming.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e # exit on errors
+
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+scramv1 project CMSSW_9_4_3
+cd CMSSW_9_4_3/src
+eval `scramv1 runtime -sh`
+
+git cms-init
+
+# 2017 electron ID's
+git cms-merge-topic lsoffi:CMSSW_9_4_0_pre3_TnP
+git cms-merge-topic guitargeek:ElectronID_MVA2017_940pre3
+scram b -j 4 # needed to have 'external' folder
+
+cd $CMSSW_BASE/external
+cd $SCRAM_ARCH
+git clone https://github.com/lsoffi/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
+cd data/RecoEgamma/ElectronIdentification/data
+git checkout CMSSW_9_4_0_pre3_TnP
+cd $CMSSW_BASE/src
+
+# KIT related packages
+git clone https://github.com/KIT-CMS/Kappa.git
+git clone https://github.com/janekbechtel/grid-control.git
+
+scram b -j 4
