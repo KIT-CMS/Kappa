347b5555e162ac26856052ea9d14fe60c90ba248
ArturAkh <artur.akhmetshin@cern.ch>
2018-10-08 19:18:26 +0200

    update 94X checkout script to recent recommendations (09.10.2018)

--- Skimming/scripts/checkoutCmssw94xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw94xPackagesForSkimming.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # exit on errors
 
+### Basic CMSSW setup ###
+
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
@@ -10,26 +12,40 @@ eval `scramv1 runtime -sh`
 
 git cms-init
 
-# Get code for electron scale & smear corrections
-git cms-merge-topic cms-egamma:EgammaPostRecoTools_940
+### Add-packages ###
 
-# Get recipes to re-correct MET (also for ECAL prefiring)
-git cms-merge-topic cms-met:METRecipe94xEEnoisePatch
+# Get DeepFlavour b-tagger ---> optional
+git cms-addpkg RecoBTag/TensorFlow
+git cherry-pick 94ceae257f846998c357fcad408986cc8a039152
 
-# KIT related packages
-git clone https://github.com/KIT-CMS/Kappa.git -b dictchanges
-git clone https://github.com/janekbechtel/grid-control.git
-
-# Get python code to rerun Tau ID's
-cd Kappa/Skimming/python
-wget https://raw.githubusercontent.com/greyxray/TauAnalysisTools/CMSSW_9_4_X_tau-pog_RunIIFall17/TauAnalysisTools/python/runTauIdMVA.py
-cd -
-
-# Get working version of HTXSRivetProducer
+# Get working version of HTXSRivetProducer ---> mainly relevant for SM HTT
 git cms-addpkg GeneratorInterface/RivetInterface
 cd GeneratorInterface/RivetInterface/plugins
 rm HTXSRivetProducer.cc
 wget https://raw.githubusercontent.com/perrozzi/cmssw/HTXS_clean/GeneratorInterface/RivetInterface/plugins/HTXSRivetProducer.cc
 cd -
 
-scram b -j 4
+### Merge-topics ###
+
+# Get code for electron scale & smear corrections
+git cms-merge-topic cms-egamma:EgammaPostRecoTools_940
+
+# Get code for electron V2 ID's (trained on 94X MC's)
+git cms-merge-topic guitargeek:EgammaID_9_4_X
+
+# Get recipes to re-correct MET (also for ECAL prefiring)
+git cms-merge-topic cms-met:METFixEE2017_949_v2
+
+# Get DPF based Tau ID (and Tau ID Embedder) ---> DPF is optional
+git cms-merge-topic ocolegro:dpfisolation # consists updated version of runTauIdMVA.py (RecoTauTag/RecoTau/python/runTauIdMVA.py). Originally, this .py file comes from https://raw.githubusercontent.com/greyxray/TauAnalysisTools/CMSSW_9_4_X_tau-pog_RunIIFall17/TauAnalysisTools/python/runTauIdMVA.py
+
+# Get latest anti-e discriminator MVA6v2 (2017 training) ---> optional
+git cms-merge-topic cms-tau-pog:CMSSW_9_4_X_tau-pog_updateAntiEDisc
+
+### Analysis group related software (ntuplizer, skimming, private MiniAOD, etc.) ###
+
+# KIT related packages
+git clone https://github.com/KIT-CMS/Kappa.git -b dictchanges
+git clone https://github.com/janekbechtel/grid-control.git
+
+scram b -j 23
