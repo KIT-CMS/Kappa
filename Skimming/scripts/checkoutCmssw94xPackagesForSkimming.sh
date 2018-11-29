daed0960b3f6ac5c403788ad5f14896c813b633e
Olena Hlushchenko <greyxray@gmail.com>
2018-11-29 23:37:31 +0100

    checkout: switch to CMSSW_9_4_12

--- Skimming/scripts/checkoutCmssw94xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw94xPackagesForSkimming.sh
@@ -6,8 +6,8 @@ set -e # exit on errors
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project CMSSW_9_4_11_cand2
-cd CMSSW_9_4_11_cand2/src
+scramv1 project CMSSW_9_4_12
+cd CMSSW_9_4_12/src
 eval `scramv1 runtime -sh`
 
 git cms-init
