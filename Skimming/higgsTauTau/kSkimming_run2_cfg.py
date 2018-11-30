9ee71e983b3a32d15de73f82dcd5487c45847a3d
Olena Hlushchenko <greyxray@gmail.com>
2018-11-30 04:20:13 +0100

    checkout: get prefiring map

--- Skimming/higgsTauTau/kSkimming_run2_cfg.py
+++ Skimming/higgsTauTau/kSkimming_run2_cfg.py
@@ -598,6 +598,17 @@ def getBaseConfig(
 				)
 
 	## ------------------------------------------------------------------------
+	process.prefiringweight = cms.EDProducer(
+		"L1ECALPrefiringWeightProducer",
+		ThePhotons=cms.InputTag("slimmedPhotons"),
+		TheJets=cms.InputTag("slimmedJets"),
+		L1Maps=cms.string("../L1Prefiring/EventWeightProducer/files/L1PrefiringMaps_new.root"),  # update this line with the location of this file
+		DataEra=cms.string("2017BtoF"),  # Use 2016BtoH for 2016
+		UseJetEMPt=cms.bool(False),  # can be set to true to use jet prefiring maps parametrized vs pt(em) instead of pt
+		PrefiringRateSystematicUncty=cms.double(0.2)  # Minimum relative prefiring uncty per object
+	)
+	process.p *= process.prefiringweight
+	## ------------------------------------------------------------------------
 
 	## GenJets
 	if not data or isEmbedded:
