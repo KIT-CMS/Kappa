1373c2d63a40fdbb63f86d58333505dff60f5a55
Daniel Savoiu <daniel.savoiu@cern.ch>
2018-12-10 18:21:44 +0100

    [Skimming] Extend setupElectrons() call signature by optional keyword argument `id_modules`.

--- Skimming/python/KElectrons_miniAOD_cff.py
+++ Skimming/python/KElectrons_miniAOD_cff.py
@@ -12,19 +12,42 @@ from PhysicsTools.SelectorUtils.tools.vid_id_tools import switchOnVIDElectronIdP
 from RecoEgamma.ElectronIdentification.egmGsfElectronIDs_cff import *
 
 
-def setupElectrons(process, electrons):
+def setupElectrons(process, electrons, id_modules=None):
+
+	# use `electrons` as source for electron objects
 	egmGsfElectronIDs.physicsObjectSrc = cms.InputTag(electrons)
+
+	# enable the electron VID producer
 	switchOnVIDElectronIdProducer(process, DataFormat.MiniAOD)
 	
+	# if no `id_modules` are given, use a sensible default selection:
+	if id_modules is None:
+		#  always include Spring15 modules (for backward compatibility)
+		id_modules = [
+			'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff',
+			'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_nonTrig_V1_cff',
+		]
 
-	my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff',
-			 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_nonTrig_V1_cff']
-	if tools.is_above_cmssw_version([8]):
-		my_id_modules.extend(['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Summer16_80X_V1_cff', 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff'])
-	if tools.is_above_cmssw_version([9,4]):
-		my_id_modules.extend(['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Fall17_94X_V1_cff', 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V1_cff', 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V1_cff'])
-	for idmod in my_id_modules:
-		setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+		# include newer electron VID modules depending on the CMSSW version
+
+		if tools.is_above_cmssw_version([8]):
+			id_modules.extend([
+				'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Summer16_80X_V1_cff',
+				'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff',
+			])
+
+		if tools.is_above_cmssw_version([9,4]):
+			id_modules.extend([
+				'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Fall17_94X_V1_cff',
+				'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V1_cff',
+				'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V1_cff',
+			])
+
+	# set up all VIDs defined in `id_modules`
+	for id_module in id_modules:
+		setupAllVIDIdsInModule(process, id_module, setupVIDElectronSelection)
+
+	# overwrite input tags to use `electrons` as physics object source
 	process.elPFIsoDepositCharged.src = cms.InputTag(electrons)
 	process.elPFIsoDepositChargedAll.src = cms.InputTag(electrons)
 	process.elPFIsoDepositNeutral.src = cms.InputTag(electrons)
