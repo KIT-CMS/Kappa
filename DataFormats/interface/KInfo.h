5539d71a3643f0394027ac68bc3cfd7ca67c2914
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-12-14 11:10:15 +0100

    add STXS to dataformat

--- DataFormats/interface/KInfo.h
+++ DataFormats/interface/KInfo.h
@@ -169,6 +169,10 @@ struct KGenEventInfo : public KEventInfo
 	double x1;            ///< x of the first parton (used for PDF reweighting)
 	double x2;            ///< x of the second parton (used for PDF reweighting)
 	double qScale;        ///< q scale of the process (used for PDF reweighting)
+	int htxs_stage0cat;   // stage 0 flag for simplified template cross section measurements
+        int htxs_stage1cat;   // stage 1 flag for simplified template cross section measurements
+        double htxs_higgsPt;  // Higgs Gen Pt from STXS tool
+        int htxs_njets30;     // njets with pt > 30 GeV from STXS tool
 	std::vector<float> lheWeight;
 
 	inline float getLheWeight(size_t index, bool failOnError = true) const
