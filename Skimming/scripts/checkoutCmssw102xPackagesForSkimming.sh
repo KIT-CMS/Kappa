1a645b1c9706b0f87e38be5d03f2684699eb6469
Olena Hlushchenko <greyxray@gmail.com>
2019-01-16 17:23:12 +0100

    10_2: add egamma recipes

--- Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
@@ -14,28 +14,29 @@ git cms-init
 
 ### Add-packages ###
 
-# TODO: conflict; check if cherry-pick needed at all
+# Jets
 # Get DeepFlavour b-tagger ---> optional
 git cms-addpkg RecoBTag/TensorFlow
+# TODO: conflict; check if cherry-pick needed at all
 #git cherry-pick 94ceae257f846998c357fcad408986cc8a039152
 
 ### Merge-topics ###
 
-# TODO: check the recipe
-# Get code for electron V2 ID's (trained on 94X MC's)
-# git cms-merge-topic cms-egamma:EgammaID_1023
+# EGamma-POG
+# https://twiki.cern.ch/twiki/bin/view/CMS/EgammaMiniAODV2
+# The 2018 MiniAOD includes the Fall17V2 IDs for electrons but not for photons.
+git cms-merge-topic cms-egamma:EgammaID_1023 # if you want the V2 IDs, otherwise skip
+git cms-merge-topic cms-egamma:EgammaPostRecoTools # just adds in an extra file to have a setup function to make things easier
 
-# TODO: confirm that not needed
-# Get code for electron scale & smear corrections
-#git cms-merge-topic cms-egamma:EgammaPostRecoTools_940
-
-# TODO: with switch to next release should be backported
+# TAU-POG
+# Note: with switch to next release should be backported
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePFTauID#Running_of_the_DNN_based_tau_ID
-# Get new DNN based Tau ID's and new Anti-Electron Disc training MVA6v3
-#git cms-merge-topic ArturAkh:DNNTauIDs_andAntiEDiscv3
 git cms-merge-topic cms-tau-pog:CMSSW_10_2_X_tau_pog_DNNTauIDs
 git clone https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles -b master RecoTauTag/TrainingFiles/data
+# Get new DNN based Tau ID's and new Anti-Electron Disc training MVA6v3
+#git cms-merge-topic ArturAkh:DNNTauIDs_andAntiEDiscv3
 
+# Prefiring
 # Ge the prefiring map
 # from private communication, valid till Moriond at least
 git cms-merge-topic lathomas:L1Prefiring_10_2_6
@@ -43,6 +44,7 @@ mkdir L1Prefiring/EventWeightProducer/data
 mv L1Prefiring/EventWeightProducer/files/L1PrefiringMaps_new.root L1Prefiring/EventWeightProducer/data/L1PrefiringMaps_new.root
 
 ### Analysis group related software (ntuplizer, skimming, private MiniAOD, etc.) ###
+
 
 # KIT related packages
 git clone git@github.com:KIT-CMS/Kappa.git -b dictchanges
