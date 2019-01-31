16541ae7cacc0b6418049f790cca92c343645536
Artur Gottmann <artur.gottmann@kit.edu>
2019-01-31 20:03:50 +0100

    Clean-up & update checkout; fix prefiring to 10_2_X, X>=10 recipe

--- Skimming/higgsTauTau/kSkimming_run2_cfg.py
+++ Skimming/higgsTauTau/kSkimming_run2_cfg.py
@@ -716,9 +716,6 @@ def getBaseConfig(
 	## ------------------------------------------------------------------------
 	process.prefiringweight = cms.EDProducer(
 		"L1ECALPrefiringWeightProducer",
-		ThePhotons=cms.InputTag("slimmedPhotons"),
-		TheJets=cms.InputTag("slimmedJets"),
-		L1Maps=cms.string("$CMSSW_BASE/src/L1Prefiring/EventWeightProducer/data/L1PrefiringMaps_new.root"),  # update this line with the location of this file
 		DataEra=cms.string("2017BtoF"),  # Use 2016BtoH for 2016
 		UseJetEMPt=cms.bool(False),  # can be set to true to use jet prefiring maps parametrized vs pt(em) instead of pt
 		PrefiringRateSystematicUncty=cms.double(0.2)  # Minimum relative prefiring uncty per object
