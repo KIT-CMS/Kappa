2e3f05ca153c14888ab9d1dc1c64ca86333cd0cf
Artur Gottmann <artur.gottmann@kit.edu>
2019-03-15 15:10:23 +0100

    checkoutCmssw102xPackagesForSkimming.sh: move to latest CMSSW release

--- Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
@@ -6,8 +6,8 @@ export SCRAM_ARCH=slc6_amd64_gcc700
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project CMSSW_10_2_10
-cd CMSSW_10_2_10/src
+scramv1 project CMSSW_10_2_12_patch1
+cd CMSSW_10_2_12_patch1/src
 eval `scramv1 runtime -sh`
 
 git cms-init
