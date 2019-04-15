55a168d1cbc681d498608e9d9d90cf3bf97b1dfd
Olena Hlushchenko <greyxray@gmail.com>
2019-04-15 20:45:26 +0200

    add producing ecalBadCalibReducedMINIAODFilter

--- Skimming/higgsTauTau/kSkimming_run2_cfg.py
+++ Skimming/higgsTauTau/kSkimming_run2_cfg.py
@@ -180,6 +180,33 @@ def getBaseConfig(
 		process.kappaTuple.ReducedTriggerObject.metfilterbits = cms.InputTag("TriggerResults", "", "MERGE")
 
 	# setup BadPFMuonFilter and BadChargedCandidateFilter
+	if tools.is_above_cmssw_version([9,4,9]):
+		process.load('RecoMET.METFilters.ecalBadCalibFilter_cfi')
+
+		baddetEcallist = cms.vuint32(
+		    [872439604,872422825,872420274,872423218,
+		     872423215,872416066,872435036,872439336,
+		     872420273,872436907,872420147,872439731,
+		     872436657,872420397,872439732,872439339,
+		     872439603,872422436,872439861,872437051,
+		     872437052,872420649,872422436,872421950,
+		     872437185,872422564,872421566,872421695,
+		     872421955,872421567,872437184,872421951,
+		     872421694,872437056,872437057,872437313,
+		     872438182,872438951,872439990,872439864,872439609, 872437181,872437182,872437053,872436794,872436667,872436536,872421541,872421413, 872421414,872421031,872423083,872421439, # extra under test
+		     ])
+
+
+		process.ecalBadCalibReducedMINIAODFilter = cms.EDFilter(
+		    "EcalBadCalibFilter",
+		    EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
+		    ecalMinEt        = cms.double(50.),
+		    baddetEcal    = baddetEcallist,
+		    taggingMode = cms.bool(True),
+		    debug = cms.bool(True)
+		    )
+		process.p *= process.ecalBadCalibReducedMINIAODFilter
+
 	if tools.is_above_cmssw_version([8]) and not tools.is_above_cmssw_version([9]):
 		process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
 		process.BadPFMuonFilter.muons = cms.InputTag("slimmedMuons")
