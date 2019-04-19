e95e92292c1eb8602b6a2a43e308437d65babd2f
Artur Gottmann <artur.gottmann@kit.edu>
2019-04-19 22:27:55 +0200

    checkout 10_2_X: update with preliminary Scale & Smear for electrons; further small updates

--- Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
@@ -6,26 +6,33 @@ export SCRAM_ARCH=slc6_amd64_gcc700
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project CMSSW_10_2_13_patch1
-cd CMSSW_10_2_13_patch1/src
+scramv1 project CMSSW_10_2_14
+cd CMSSW_10_2_14/src
 eval `scramv1 runtime -sh`
 
 git cms-init
 
 ### Add-packages ###
+git cms-addpkg EgammaAnalysis/ElectronTools
+rm EgammaAnalysis/ElectronTools/data -rf
+git clone git@github.com:cms-egamma/EgammaAnalysis-ElectronTools.git EgammaAnalysis/ElectronTools/data
+cd EgammaAnalysis/ElectronTools/data
+git checkout ScalesSmearing2018_Dev
+cd -
 
 ### Merge-topics ###
-git cms-merge-topic ArturAkh:stxs_1p1 # adapted HTXS code for stage 1.1; used reference: https://gitlab.cern.ch/LHCHIGGSXS/LHCHXSWG2/STXS
+git cms-merge-topic KIT-CMS:stxs_1p1 # adapted HTXS code for stage 1.1; used reference: https://gitlab.cern.ch/LHCHIGGSXS/LHCHXSWG2/STXS
 
 # EGamma-POG
 # https://twiki.cern.ch/twiki/bin/view/CMS/EgammaMiniAODV2, https://twiki.cern.ch/twiki/bin/view/CMS/EgammaPostRecoRecipes
 # The 2018 MiniAOD includes the Fall17V2 IDs for electrons but not for photons.
 git cms-merge-topic cms-egamma:EgammaPostRecoTools # just adds in an extra file to have a setup function to make things easier
+git cms-merge-topic cms-egamma:EgammaPostRecoTools_dev # for 2018 Scale & Smear
 
 ### Analysis group related software (ntuplizer, skimming, private MiniAOD, etc.) ###
 
 # KIT related packages
-git clone git@github.com:KIT-CMS/Kappa.git -b dictchanges_10_2
+git clone git@github.com:KIT-CMS/Kappa.git -b dictchanges
 git clone git@github.com:janekbechtel/grid-control.git
 
 CORES=`grep -c ^processor /proc/cpuinfo`
