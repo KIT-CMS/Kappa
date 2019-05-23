b1bf939e6313ca69bf75bfd7b9436dd0b9aece99
Sebastian Brommer <sebastian.brommer@student.kit.edu>
2019-05-23 11:22:33 +0200

    better method of add recepie

--- Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
@@ -32,10 +32,8 @@ git cms-merge-topic cms-egamma:EgammaPostRecoTools_dev # for 2018 Scale & Smear
 ### Analysis group related software (ntuplizer, skimming, private MiniAOD, etc.) ###
 
 # KIT related packages
-git clone git@github.com:KIT-CMS/Kappa.git -b dictchanges
+git clone --recursive git@github.com:KIT-CMS/Kappa.git -b dictchanges
 git clone git@github.com:KIT-CMS/grid-control.git
-
-git submodule update --init --recursive # add the datasets.json submodule
 
 CORES=`grep -c ^processor /proc/cpuinfo`
 scram b -j $CORES
