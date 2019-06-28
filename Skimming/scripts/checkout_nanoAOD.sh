661b63d4ed9ac790b645e2ea8d5cf79208307bc5
Artur Gottmann <artur.gottmann@kit.edu>
2019-06-28 13:53:33 +0200

    checkout_nanoAOD.sh: adding checkout for nanoAOD production

---
+++ Skimming/scripts/checkout_nanoAOD.sh
@@ -0,0 +1,16 @@
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+scramv1 project CMSSW_10_2_15_patch2
+cd CMSSW_10_2_15_patch2/src/
+
+eval `scramv1 runtime -sh`
+git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_DeepTau2017v2
+
+wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/DeepTau2017v2/DeepTauId/deepTau_2017v2p6_e6_core.pb -P RecoTauTag/TrainingFiles/data/DeepTauId
+wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/DeepTau2017v2/DeepTauId/deepTau_2017v2p6_e6_inner.pb -P RecoTauTag/TrainingFiles/data/DeepTauId
+wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/DeepTau2017v2/DeepTauId/deepTau_2017v2p6_e6_outer.pb -P RecoTauTag/TrainingFiles/data/DeepTauId
+
+git clone --recursive git@github.com:KIT-CMS/Kappa.git -b nanoAOD
+git clone git@github.com:KIT-CMS/grid-control
+
+scram b -j 12
