dee10ea477fe58c0ae24dbfee48658e857e9a4be
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2019-08-28 18:20:06 +0200

    apply hotfix of deep tau ID version in checkout script

--- Skimming/scripts/checkout_nanoAOD.sh
+++ Skimming/scripts/checkout_nanoAOD.sh
@@ -7,6 +7,9 @@ eval `scramv1 runtime -sh`
 git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_DeepTau2017v2
 git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_deepTauVetoPCA
 
+sed 's!idDeepTau2017v2!idDeepTau2017v2p1!g' PhysicsTools/NanoAOD/python/taus_cff.py -i
+sed 's!rawDeepTau2017v2!rawDeepTau2017v2p1!g' PhysicsTools/NanoAOD/python/taus_cff.py -i
+
 git clone --recursive git@github.com:KIT-CMS/Kappa.git -b nanoAOD
 git clone git@github.com:KIT-CMS/grid-control
 git clone git@github.com:janekbechtel/grid-control jb_grid-control
