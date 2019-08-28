b2c33666649d367be867d43df7fe891a62326c87
Janek Bechtel <janek.bechtel@student.kit.edu>
2019-08-28 15:21:40 +0200

    Update checkout_nanoAOD.sh

--- Skimming/scripts/checkout_nanoAOD.sh
+++ Skimming/scripts/checkout_nanoAOD.sh
@@ -4,7 +4,7 @@ scramv1 project CMSSW_10_2_15_patch2
 cd CMSSW_10_2_15_patch2/src/
 
 eval `scramv1 runtime -sh`
-git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_DeepTau2017v2
+git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_deepTauVetoPCA
 
 wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/DeepTau2017v2/DeepTauId/deepTau_2017v2p6_e6_core.pb -P RecoTauTag/TrainingFiles/data/DeepTauId
 wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/DeepTau2017v2/DeepTauId/deepTau_2017v2p6_e6_inner.pb -P RecoTauTag/TrainingFiles/data/DeepTauId
