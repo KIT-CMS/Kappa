4aaa6c1670df81edc3197577ea086ef9a732db1d
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2019-10-10 18:40:46 +0200

    update checkout script to newer CMSSW version that does not need deepTauID merge topic

--- Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw102xPackagesForSkimming.sh
@@ -6,8 +6,8 @@ export SCRAM_ARCH=slc6_amd64_gcc700
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project CMSSW_10_2_16_patch1
-cd CMSSW_10_2_16_patch1/src
+scramv1 project CMSSW_10_2_17
+cd CMSSW_10_2_17/src
 eval `scramv1 runtime -sh`
 
 git cms-init
@@ -24,10 +24,6 @@ git clone git@github.com:cms-data/EgammaAnalysis-ElectronTools.git EgammaAnalysi
 # The 2018 MiniAOD includes the Fall17V2 IDs for electrons but not for photons.
 git cms-merge-topic cms-egamma:EgammaPostRecoTools # just adds in an extra file to have a setup function to make things easier
 git cms-merge-topic cms-egamma:slava77-btvDictFix_10210 # fixes the Run2018D dictionary issue, see https://github.com/cms-sw/cmssw/issues/26182, may not be necessary for later releases
-
-# Tau-POG
-# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePFTauID#Running_of_the_DeepTauIDs_ver_20
-git cms-merge-topic -u cms-tau-pog:CMSSW_10_2_X_tau-pog_DeepTau2017v2p1_nanoAOD
 
 ### Analysis group related software (ntuplizer, skimming, private MiniAOD, etc.) ###
 
