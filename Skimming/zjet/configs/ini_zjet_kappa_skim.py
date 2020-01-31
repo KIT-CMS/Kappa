5362cf5409f6abce20a70288893dfb063c3ee7f9
Christoph Heidecker <cheidecker@bms1.etp.kit.edu>
2020-01-31 12:54:09 +0100

    Fixed syntax error in ini script

--- Skimming/zjet/configs/ini_zjet_kappa_skim.py
+++ Skimming/zjet/configs/ini_zjet_kappa_skim.py
@@ -2,10 +2,10 @@
 
 echo "Set up user specific Kappa settings!"
 
-if [ ${USER} == "cheidecker" ]:
+if [ ${USER} == "cheidecker" ]; then
     KAPPA_SE_PATH="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/cheideck/Skimming/" 
     KAPPA_WORK_DIR="/portal/ekpbms1/home/cheidecker/kappa_work"
-elif [ ${USER} == "dsavoiu" ]:
+elif [ ${USER} == "dsavoiu" ]; then
     KAPPA_SE_PATH="srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/dsavoiu/Skimming/"
     KAPPA_WORK_DIR="/portal/ekpbms1/home/dsavoiu/kappa_work/"
 else:
