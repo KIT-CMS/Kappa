664cd0e0e990eba1508bd0344f9747073b2a23df
Christoph Heidecker <christoph.heidecker@cern.ch>
2020-02-25 17:01:30 +0100

    Made dump of CMSSW config default for testing

--- Skimming/zjet/configs/jec17ul/1060_DYJetsToLL_Summer19-madgraphMLM_mc2017_realistic_v6-v3/testKappaSkim.sh
+++ Skimming/zjet/configs/jec17ul/1060_DYJetsToLL_Summer19-madgraphMLM_mc2017_realistic_v6-v3/testKappaSkim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 _GT="106X_mc2017_realistic_v6"
-_NEVT=1000
+_NEVT=100
 _GRID_PATH_PREFIX="root://xrootd-cms.infn.it/"
 
 _FILE="/store/mc/RunIIFall17MiniAOD/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/20000/FECF3F74-6CD7-E711-8B51-A4BF0112BC02.root"
@@ -15,7 +15,7 @@ cmsRun ../kappaSkim_default_cfg.py inputFiles=${_GRID_PATH_PREFIX}${_FILE} \
                         isData=0 \
                         outputFile=testKappaSkim_out_${_GT}.root \
                         maxEvents=${_NEVT} \
-                        dumpPythonAndExit=0 2>&1 | tee cout_${_GT}.log
+                        dumpPython=1 2>&1 | tee cout_${_GT}.log
 mv cout_$_GT.log $_dir/
 mv infos.log $_dir/
 mv debugs.log $_dir/
