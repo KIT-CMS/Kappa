8667ae663e34c3938da1c9e4285aed3d453073e6
R. Florian von Cube <ralf.florian.von.cube@cern.ch>
2020-07-10 12:19:55 +0200

    Initial configs for DYJetsToLL MC UL18.

---
+++ Skimming/zjet/configs/jec18ul/106X_DYJetsToLL_Summer19-madgraphMLM_upgrade2018_realistic_v11_L1v1-v1/testKappaSkim.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+_GT="106X_upgrade2018_realistic_v11_L1v1"
+_NEVT=100
+_IS_DATA=false
+_GRID_PATH_PREFIX="root://xrootd-cms.infn.it/"
+_FILE="/store/mc/RunIISummer19UL18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v11_L1v1-v1/70000/FFEDB6ED-A3B9-0D41-B529-7FE74490AEFD.root"
+_DIR="test/${_GT}"
+
+mkdir -p $_DIR
+
+echo "##############################################################################"
+echo "STARTING cmsRun !!!!"
+echo "##############################################################################"
+
+cmsRun ../kappaSkim_default_cfg.py inputFiles=${_GRID_PATH_PREFIX}${_FILE} \
+                        globalTag=${_GT} \
+                        isData=${_IS_DATA} \
+                        outputFile=testKappaSkim_out_${_GT}.root \
+                        maxEvents=${_NEVT} \
+                        dumpPython=1 2>&1 | tee cout_${_GT}.log
+
+echo "##############################################################################"
+echo "cmsRun terminated !!!!"
+echo "##############################################################################"
+
+if [ -f cout_$_GT.log ]; then
+    mv cout_$_GT.log $_DIR/
+fi
+
+if [ -f infos.log ]; then
+    mv infos.log $_DIR/
+fi
+
+if [ -f debugs.log ]; then
+    mv debugs.log $_DIR/
+fi
+
+if [ -f testKappaSkim_out_${_GT}_numEvent${_NEVT}.root ]; then
+    mv testKappaSkim_out_${_GT}_numEvent${_NEVT}.root $_DIR/
+fi
+
