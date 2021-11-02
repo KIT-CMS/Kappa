beef9eb3c6765af249d1c1dff20520089985b3b2
RHofsaess <uaikd@student.kit.edu>
2021-11-02 12:55:22 +0100

    [UL16] Moved to MiniAODv2

---
+++ Skimming/zjet/configs/jec16ul/106X_DoubleEG_Run2016_UL2016_MiniAODv2_postVFP/testKappaSkim.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+_GT="106X_dataRun2_v35"
+_NEVT=100
+_IS_DATA=true
+_GRID_PATH_PREFIX="root://cms-xrd-global.cern.ch:1094/"
+_FILE="/store/data/Run2016F/DoubleEG/MINIAOD/UL2016_MiniAODv2-v1/120000/02D20025-B7D3-414F-9EE8-FC29B158542D.root"
+_DIR="test/${_GT}"
+
+mkdir -p $_DIR
+
+echo "##############################################################################"
+echo "STARTING cmsRun !!!!"
+echo "##############################################################################"
+
+cmsRun ../kappaSkim_default_cfg_postVFP.py inputFiles=${_GRID_PATH_PREFIX}${_FILE} \
+                        globalTag=${_GT} \
+                        isData=${_IS_DATA} \
+                        outputFile=testKappaSkim_out_${_GT}.root \
+                        maxEvents=${_NEVT} \
+                        dumpPython=1 2>&1 | tee cout_${_GT}.log
+
+echo "##############################################################################"
+echo "cmsRun terminated !!!!"
+echo "##############################################################################"
+
+if [ -f cout_$_GT.log ]; then
+    mv cout_$_GT.log $_DIR/
+fi
+
+if [ -f infos.log ]; then
+    mv infos.log $_DIR/
+fi
+
+if [ -f debugs.log ]; then
+    mv debugs.log $_DIR/
+fi
+
+if [ -f testKappaSkim_out_${_GT}_numEvent${_NEVT}.root ]; then
+    mv testKappaSkim_out_${_GT}_numEvent${_NEVT}.root $_DIR/
+fi
