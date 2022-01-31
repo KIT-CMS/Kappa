59cbb83e32ba8685c2feffd832b50dacc74c969d
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2022-01-31 16:53:44 +0100

    add skimming test-scripts for UL17 datasets

---
+++ Skimming/zjet/configs/zjet17ul/DYJetsToLL/testKappaSkim.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+_GT="106X_mc2017_realistic_v9"
+_NEVT=100
+_IS_DATA=false
+_GRID_PATH_PREFIX="root://xrootd-cms.infn.it/"
+_FILE="/store/mc/RunIISummer20UL17MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/106X_mc2017_realistic_v9-v2/40000/0066C701-8B8A-3F42-AB75-9CF2FB14038A.root"
+_DIR="test/${_GT}"
+
+mkdir -p $_DIR
+
+echo "##############################################################################"
+echo "STARTING cmsRun !!!!"
+echo "##############################################################################"
+
+cmsRun ../kappaSkim_default_cfg.py inputFiles=${_GRID_PATH_PREFIX}${_FILE} \
+                        globalTag=${_GT} \
+                        isData=${_IS_DATA} \
+                        outputFile=testKappaSkim_out_${_GT}.root \
+                        maxEvents=${_NEVT} \
+                        dumpPython=1 2>&1 | tee cout_${_GT}.log
+
+echo "##############################################################################"
+echo "cmsRun terminated !!!!"
+echo "##############################################################################"
+
+if [ -f cout_$_GT.log ]; then
+    mv cout_$_GT.log $_DIR/
+fi
+
+if [ -f infos.log ]; then
+    mv infos.log $_DIR/
+fi
+
+if [ -f debugs.log ]; then
+    mv debugs.log $_DIR/
+fi
+
+if [ -f testKappaSkim_out_${_GT}_numEvent${_NEVT}.root ]; then
+    mv testKappaSkim_out_${_GT}_numEvent${_NEVT}.root $_DIR/
+fi
